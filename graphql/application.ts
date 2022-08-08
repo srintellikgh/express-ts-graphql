ce97a09e2d7bd631d4b0f370e5ab08a513805680
intelligh <intelligh.76@gmail.com>
2022-08-08 19:33:38 -0400

    added post app

--- graphql/application.ts
+++ graphql/application.ts
@@ -1,9 +1,10 @@
 import { createApplication } from 'graphql-modules';
 import { authModule } from './auth';
+import { postModule } from './post';
 import { userModule } from './user';
 
 const application = createApplication({
-  modules: [authModule, userModule],
+  modules: [authModule, postModule, userModule],
 });
 
 const { schema } = application;
